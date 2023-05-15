0edfac5a6c5336a3e9a38d5c9e650b0a4aa4ab2d
edubew <edubew98@gmail.com>
2023-05-15 18:22:54 +0300

    Write a method to decode a single character and a word from the morse table

--- decode.rb
+++ decode.rb
@@ -37,3 +37,21 @@ MORSE_TABLE = {
     '8' => '---..',
     '9' => '----.'
 }.freeze
+
+# Define a method to decode a single character from the morse table
+def decode_char(char)
+    MORSE_TABLE[char]
+end
+
+# Define a method to decode a word from morse table
+def decode_word(word)
+    decoded_word = ''
+
+    # Map each character in morse table to it's corresponding letter in the word
+    word.split.each do |char|
+        decoded_word += decode_char(char)
+    end
+
+    # Return the decoded word
+    decoded_word
+end
